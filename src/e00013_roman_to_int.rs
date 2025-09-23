5eb1835f87836101b596e317b4c5ff7770d3d0f4
Victor Campello <victor.campello.work@gmail.com>
2025-09-23 17:26:12 +0100

    feat(13): solve roman-to-int

---
+++ src/e00013_roman_to_int.rs
@@ -0,0 +1,59 @@
+pub struct Solution {}
+
+impl Solution {
+    fn to_int(s: &char) -> i32 {
+        match s {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => panic!("Could not find value for {s}"),
+        }
+    }
+
+    pub fn roman_to_int(s: String) -> i32 {
+        let mut total: i32 = 0;
+        let mut it = s.chars().peekable();
+
+        while let Some(curr) = it.next() {
+            let curr_int = Solution::to_int(&curr);
+
+            match it.peek() {
+                Some(next) if curr_int < Solution::to_int(next) => {
+                    total = total - curr_int + Solution::to_int(next);
+                    it.next();
+                }
+                // `Some(next) if` is not exhaustive
+                _ => total += curr_int,
+            }
+        }
+
+        total
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    fn run(s: String) -> i32 {
+        Solution::roman_to_int(s)
+    }
+
+    #[test]
+    fn case_1() {
+        assert_eq!(run("III".to_string()), 3)
+    }
+
+    #[test]
+    fn case_2() {
+        assert_eq!(run("LVIII".to_string()), 58)
+    }
+
+    #[test]
+    fn case_3() {
+        assert_eq!(run("MCMXCIV".to_string()), 1994)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod e00012_int_to_roman;
+pub mod e00013_roman_to_int;
